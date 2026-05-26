09a3e7ba2fcf86d51a34cafe2bce62702d39aeef
Ta-syn Islam Nishan <140194155+ta-syn@users.noreply.github.com>
2026-05-26 19:14:53 +0600

    Fix debug signing and update README.md

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,6 @@ android {
       keyAlias = "upload"
       keyPassword = System.getenv("KEY_PASSWORD")
     }
-    create("debugConfig") {
-      storeFile = file("${rootDir}/debug.keystore")
-      storePassword = "android"
-      keyAlias = "androiddebugkey"
-      keyPassword = "android"
-    }
   }
 
   buildTypes {
@@ -44,7 +38,7 @@ android {
       signingConfig = signingConfigs.getByName("release")
     }
     debug {
-      signingConfig = signingConfigs.getByName("debugConfig")
+      // Uses the default auto-generated Android debug signing config
     }
   }
   compileOptions {
